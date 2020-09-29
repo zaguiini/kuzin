9b1ffd3bcdc2714e22ef3ba0556153ac54e628fd
HELIO POTELICKI <helio.p2000@hotmail.com>
2020-09-28 21:55:44 -0300

    fontes gals em java

---
+++ Fontes_Gals_JAVA/SyntaticError.java
@@ -0,0 +1,9 @@
+public class SyntaticError extends AnalysisError {
+    public SyntaticError(String msg, int position) {
+        super(msg, position);
+    }
+
+    public SyntaticError(String msg) {
+        super(msg);
+    }
+}
