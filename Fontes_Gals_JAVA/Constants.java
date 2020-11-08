033db32dd8b0d3797e1b92873683ce670717694e
HELIO POTELICKI <helio.p2000@hotmail.com>
2020-11-08 14:17:37 -0300

    Fontes gals java

---
+++ Fontes_Gals_JAVA/Constants.java
@@ -0,0 +1,48 @@
+public interface Constants extends ScannerConstants, ParserConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_id = 2;
+    int t_c_int = 3;
+    int t_c_float = 4;
+    int t_c_str = 5;
+    int t_int = 6;
+    int t_float = 7;
+    int t_str = 8;
+    int t_and = 9;
+    int t_or = 10;
+    int t_not = 11;
+    int t_if = 12;
+    int t_elif = 13;
+    int t_else = 14;
+    int t_for = 15;
+    int t_in = 16;
+    int t_range = 17;
+    int t_while = 18;
+    int t_end = 19;
+    int t_false = 20;
+    int t_true = 21;
+    int t_input = 22;
+    int t_print = 23;
+    int t_TOKEN_24 = 24; //"("
+    int t_TOKEN_25 = 25; //")"
+    int t_TOKEN_26 = 26; //"=="
+    int t_TOKEN_27 = 27; //"!="
+    int t_TOKEN_28 = 28; //"<"
+    int t_TOKEN_29 = 29; //"<="
+    int t_TOKEN_30 = 30; //">"
+    int t_TOKEN_31 = 31; //">="
+    int t_TOKEN_32 = 32; //"+"
+    int t_TOKEN_33 = 33; //"-"
+    int t_TOKEN_34 = 34; //"*"
+    int t_TOKEN_35 = 35; //"/"
+    int t_TOKEN_36 = 36; //"//"
+    int t_TOKEN_37 = 37; //"%"
+    int t_TOKEN_38 = 38; //","
+    int t_TOKEN_39 = 39; //":"
+    int t_TOKEN_40 = 40; //"="
+    int t_TOKEN_41 = 41; //"+="
+    int t_TOKEN_42 = 42; //"-="
+
+}
